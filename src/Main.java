026090198f0cc1a8588c9fec8ff11aeef4d89968
laptop <qoarhfl@gmail.com>
2023-09-06 15:50:06 +0900

    프로그래머스 - 카카오 아이디 성공!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,50 @@
 public class Main {
     public static void main(String[] args) {
 
+        // static class
+        System.out.println(StaticClassTest.staticField);
+        StaticClassTest.staticMethod();
+
+        System.out.println(new StaticClassTest().nonStaticField);
+        new StaticClassTest().nonStaticMethod();
+
+        // non-static class
+        System.out.println(NonStaticClassTest.staticField);
+        NonStaticClassTest.staticMethod();
+
+        Main mainClass = new Main();
+        System.out.println(mainClass.new NonStaticClassTest().nonStaticField);
     }
+
+    static class StaticClassTest {
+
+        private String nonStaticField = "nonStaticField";
+
+        private static String staticField = "staticField";
+
+        public void nonStaticMethod(){
+            System.out.println("nonStaticMethod");
+        }
+
+        public static void staticMethod(){
+            System.out.println("staticMethod");
+        }
+
+    }
+
+    class NonStaticClassTest {
+
+        private String nonStaticField = "nonStaticField";
+
+        private static String staticField = "staticField";
+
+        public void nonStaticMethod(){
+            System.out.println("nonStaticMethod");
+        }
+
+        public static void staticMethod(){
+            System.out.println("staticMethod");
+        }
+    }
+
 }
